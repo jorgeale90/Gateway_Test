87afc50881c6e8101e89e10fd3c3837edb08b8e8
jorgeale90 <jorgealec90@gmail.com>
2023-11-24 02:31:33 -0500

    Change Repository

--- src/services/DeleteGatewayService.ts
+++ src/services/DeleteGatewayService.ts
@@ -1,18 +1,29 @@
 import { getCustomRepository } from "typeorm";
-import { Gateway } from "../entities";
+import { Gateway, Peripheral } from "../entities";
 import { GatewayRepository } from "../repositories/GatewayRepository";
+import { PeripheralRepository } from "../repositories/PeripheralRepository";
 
 class DeleteGatewayService {
   async delete(id: string) {
     const gatewayRepository = getCustomRepository(GatewayRepository);
+    // Gets the peripheral repository
+    const peripheralRepository = getCustomRepository(PeripheralRepository);
 
+    // Eliminate the peripherals associated with the gateway
+    await peripheralRepository
+        .createQueryBuilder()
+        .delete()
+        .from(Peripheral)
+        .where("gatewayId = :id", { id })
+        .execute();
+
+    // Delete the gateway
     return await gatewayRepository
         .createQueryBuilder()
         .delete()
         .from(Gateway)
         .where("id = :id", { id })
         .execute();
-
   }
 }
 
